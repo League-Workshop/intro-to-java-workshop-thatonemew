41148e2c999cd84c7cb4b08946f11171e4c76d47
League Student <league@iMac-8.attlocal.net>
2018-12-26 13:23:35 -0800

    first day

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,18 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	    Robot aLpha1 = new Robot();
+	    aLpha1.setSpeed(200);
+	    aLpha1.penDown();
+	    aLpha1.turn (90);
+	    aLpha1.setPenColor(Color.cyan);
+	    aLpha1.move(91);
+		for (int o = 0; o<4 ; o++) {
+	    aLpha1.setSpeed(200);
+		aLpha1.turn (90);
+		aLpha1.setPenColor(Color.cyan);
+		aLpha1.move(91);	
+		}
 	
-		
-		
 	}
 }
